ee180f15c7cc84031e00378d94388773178ea81c
hkang4628 <hkang4628@gmail.com>
2023-03-11 12:43:56 +0900

    s3 bucket 코드 생성

---
+++ terraform/flower53/s3_bucket.tf
@@ -0,0 +1,29 @@
+# S3 설정 부분
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "flower53-image-bucket"
+
+  tags = {
+    Name        = "Image bucket"
+  }
+
+  # Buckets cannot be deleted until they are empty. By default, Terraform will
+  # delete buckets before deleting their contents, but this behaviour can be
+  # changed with the `force_destroy` parameter.
+  force_destroy = true
+}
+
+
+
+resource "aws_s3_bucket_public_access_block" "s3_bucket_public_access" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_acl" "s3_bucket_acl" {
+  bucket  = aws_s3_bucket.s3_bucket.id
+  acl     = "private"
+}
