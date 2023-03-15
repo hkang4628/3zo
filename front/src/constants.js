27d06d07029cd16c591cf2978781902406e97030
hkang4628 <hkang4628@gmail.com>
2023-03-16 01:35:13 +0900

    idc fornt 배포 2

--- front/src/constants.js
+++ front/src/constants.js
@@ -1,5 +1,6 @@
 // IDC용
-export const SERVER_URL = "was-service.default.svc.cluster.local:8000/proxy";
+export const SERVER_URL =
+  "http://was-service.default.svc.cluster.local:8000/proxy";
 
 // AWS용
 // export const SERVER_URL = "http://web-lb-329549199.us-east-1.elb.amazonaws.com/proxy";
