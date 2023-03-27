8fb7ffd554ea0dc1c1b7e7f4427ff2a2e9f5d6fe
hkang4628 <122418328+hkang4628@users.noreply.github.com>
2023-03-28 03:44:26 +0900

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,22 @@
 provider "aws" {
   region = var.aws_region
   #   shared_credentials_files = ["./credentials/credentials"]
-  shared_credentials_files = ["C://Users//Administrator//.aws//credentials"]
+  #   shared_credentials_files = ["C://Users//Administrator//.aws//credentials"]
+        access_key = var.aws_access_key
+        secret_key = var.aws_secret_key
 }
 
 resource "aws_key_pair" "my_keypair" {
   key_name   = var.key_name
   public_key = file("C://Users//Administrator//.ssh//id_rsa.pub")
+}
+
+variable "aws_access_key" {
+  type        = string
+  description = "AWS access key"
+}
+
+variable "aws_secret_key" {
+  type        = string
+  description = "AWS secret key"
 }
